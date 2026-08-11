8b6a593d3af835fbabc30ead99d0d366954e8a52
ASHISH ANAND <100472956+Ashish-7529@users.noreply.github.com>
2026-08-11 16:51:23 +0530

    Time: 2 ms (75.60%) | Memory: 18.2 MB (70.87%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+
+        vector<vector<int>> adj(n);
+        vector<int> out, ind(n, 0);
+
+        priority_queue<int, vector<int>, greater<int>> q;
+        for (auto& it : prerequisites) {
+            adj[it[1]].push_back(it[0]);
+            ind[it[0]]++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (ind[i] == 0) {
+                q.push(i);
+            }
+        }
+        while (!q.empty()) {
+            int c = q.top();
+            q.pop();
+
+            out.push_back(c);
+
+            for (int i : adj[c]) {
+                ind[i]--;
+
+                if (ind[i] == 0) {
+                    q.push(i);
+                }
+            }
+        }
+        if (out.size() != n) {
+            return {};
+        }
+
+        return out;
+    }
+};
